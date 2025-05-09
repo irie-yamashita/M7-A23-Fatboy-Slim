c40f0bf0abc5d4df247c90da1c09d7c09f9c7c8c
irie <irie.yamashita.7e8@itb.cat>
2025-05-09 08:46:28 +0000

    Updata index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,16 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$db = new SQLite3 ('./../Slim/data/artistes.db');
 
 // Instantiate App
 $app = AppFactory::create();
@@ -13,9 +20,18 @@ $app->addErrorMiddleware(true, true, true);
 
 // Add routes
 $app->get('/', function (Request $request, Response $response) {
-    $htmlContent = "
-    <p>Hola</p>
-    ";
+    /*$stmt = $db->query("SELECT * FROM artistes");
+    $result = $this->db->query($stmt);
+    $artistes = [];
+    while($artista = $result->fetchArray(SQLITE3_ASSOC))
+        $artistes[] = $artista;
+    return $artista;
+
+    foreach ($artistas as $artista) {
+        $htmlContent .= $artista['nom'];
+    }*/
+
+    $htmlContent = 'Hola';
 
     $response -> getBody()->write($htmlContent);
     return $response->withHeader('Content-Type', 'text/html');
